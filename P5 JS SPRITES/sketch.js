fc9b4b7052ba1631e1231ac8baf199315ec885ad
unknown <raveen.anand@whitehatjr.com>
2021-10-23 10:59:37 +0530

    v2

---
+++ P5 JS SPRITES/sketch.js
@@ -0,0 +1,23 @@
+//1. Create a variable named "box"
+
+function setup() {
+  createCanvas(400, 400);
+  
+  //2.Assign "box" to the "createSprite()"
+  
+  //4.Set sprite color to "white"
+  
+  //5.set velocity of sprite for horizontal movement
+  
+}
+function draw() {
+  background("orange");
+  
+  //3. Display sprite using "drawSprites()"
+ 
+}
+
+
+
+
+  
